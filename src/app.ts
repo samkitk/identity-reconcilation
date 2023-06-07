32864ab6ecfea7f27a72cf1b76c7e7ddd6134726
Samkit <28357583+samkitk@users.noreply.github.com>
2023-06-07 16:13:11 +0530

    fix: beautified / path get request response

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { logger } from "./helpers/winston";
 import { host_environment, server_port } from "./helpers/environment";
+import git from "git-last-commit";
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  git.getLastCommit((err, commit) => {
+    if (err) {
+      logger.log("error", err);
+      return res.send("Hello World!");
+    }
+    return res.send("Hello World! " + commit.shortHash);
+  });
 });
 
 app.listen(server_port, () => {
