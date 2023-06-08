e075c415e9671470ba89cf45b9f3372373d00488
Samkit <28357583+samkitk@users.noreply.github.com>
2023-06-08 13:37:59 +0530

    feat: tests working

---
+++ tests/getPrimartContactFromEmail.spec.ts
@@ -0,0 +1,11 @@
+import { getPrimaryContactFromEmailAndPhoneNumber } from "../src/identity/contact";
+
+test("getPrimaryContactFromEmail should retrieve the primary contact with the given email", async () => {
+  const contact = await getPrimaryContactFromEmailAndPhoneNumber(
+    "test@example.com"
+  );
+  expect(contact).toBeDefined();
+  expect(contact?.email).toBe("test@example.com");
+  expect(contact?.phoneNumber).toBe("1234567890");
+  expect(contact?.linkPrecedence).toBe("PRIMARY" || "SECONDARY");
+});
